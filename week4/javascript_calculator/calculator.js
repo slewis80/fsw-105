85bb63f4309063a1d66a8a9e441864c64b9906b1
Sabrina <sabrina.lewis@bryanuniversity.edu>
2020-03-04 20:18:11 -0600

    javascript calculator

---
+++ week4/javascript_calculator/calculator.js
@@ -0,0 +1,20 @@
+var readlineSync = require('readline-sync');
+
+// Wait for user's response.
+var num1 = readlineSync.questionInt('Please enter your first number: ');
+var num2 = readlineSync.questionInt('Please enter your second number: ');
+var operation = readlineSync.question('Please enter the operation to perform: add, sub, mul, div: ');
+
+// functions
+if (operation === "add") {
+    console.log("The result is: " + (num1 + num2));
+}
+if (operation === "sub") {
+    console.log("The result is: " + num1 - num2);
+}
+if (operation === "mul") {
+    console.log("The result is: " + num1 * num2);
+}
+if (operation === "div") {
+    console.log("The result is: " + num1 / num2);
+}
